140e4884eb699245a39bf9bd962de8f3e7c731aa
Henry-Azer <henryazer@outlook.com>
2022-08-27 17:04:27 +0200

    first commit

---
+++ src/components/ActiveElement.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h2>{{ topicTitle ? topicTitle : "Topic Picker" }}</h2>
+    <p>{{ text ? text : "You can choose a topic form topics grid to display it here, It's created to implement passing and emitting elements through more than component using provide, inject." }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['topicTitle', 'text'],
+};
+</script>
